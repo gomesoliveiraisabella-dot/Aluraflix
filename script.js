02af7769aef0290d20f4e196d429d3dab9a281da
gomesoliveiraisabella-dot <gomes.oliveira.isabella@escola.pr.gov.br>
2026-05-28 10:55:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const form = document.getElementById("contactForm");
+const message = document.getElementById("message");
 
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  message.textContent =
+    "Mensagem enviada com sucesso! Entraremos em contato.";
+
+  form.reset();
+
+  setTimeout(() => {
+    message.textContent = "";
+  }, 4000);
+});
